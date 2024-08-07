589908da31c5e1e7070c7a457b0b85045b22c3ae
John <johnpk1007@gmail.com>
2024-08-07 12:33:37 +0900

    add ripple

---
+++ components/artistLinkButton.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { Context } from "./provider";
+import { useContext } from "react";
+import { useRouter } from "next/navigation";
+
+export const ArtistLinkButton = ({ artist, page, extra }) => {
+  const { data, num } = useContext(Context);
+  const router = useRouter();
+  const handleClick = () => {
+    sessionStorage.setItem("data", JSON.stringify(data));
+    sessionStorage.setItem("height", `${window.scrollY}`);
+    sessionStorage.setItem("num", `${num}`);
+    router.push(`/artist/${artist}`);
+  };
+  return (
+    <div onClick={handleClick} className="rounded-r-lg hover:bg-slate-100">
+      {extra}
+      {extra === "Featuring" || extra === "&" || extra === "X" || extra === "x"
+        ? " "
+        : ""}
+      {artist}
+      {page.page ? (
+        <div className="absolute xxs:left-[-15px] sm:left-[-25px] lg:left-[-30px] xxs:w-[15px] sm:w-[25px] lg:w-[30px] top-0 rounded-l-lg top-0 bottom-0 bg-inherit">
+          +
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
--- components/editedTitle.js
+++ components/editedTitle.js
@@ -1,4 +1,5 @@
 import { LinkButton } from "./linkButton";
+import { ArtistLinkButton } from "./artistLinkButton";
 
 const conditions = [",", "Featuring", "&", "X", "x"];
 const separators = /([,&]|Featuring| X | x )/;
@@ -50,8 +51,9 @@ export const EditedTitle = ({ artist, page }) => {
                 key={idx}
                 className="flex items-center font-poppins font-light lg:text-2xl xl:text-3xl  relative"
               >
-                {newArrItem[0]}
-                {page[idx].page ? <LinkButton artist={page[idx].artist} /> : ""}
+                {/* {newArrItem[0]}
+                {page[idx].page ? <LinkButton artist={page[idx].artist} /> : ""} */}
+                <ArtistLinkButton artist={newArrItem[0]} page={page[idx]} />
               </div>
             );
           } else {
@@ -60,7 +62,7 @@ export const EditedTitle = ({ artist, page }) => {
                 key={idx}
                 className="flex items-center font-poppins font-light lg:text-2xl xl:text-3xl relative"
               >
-                {newArrItem[0]}
+                {/* {newArrItem[0]}
                 {newArrItem[0] === "Featuring" ||
                 newArrItem[0] === "&" ||
                 newArrItem[0] === "X" ||
@@ -68,7 +70,12 @@ export const EditedTitle = ({ artist, page }) => {
                   ? " "
                   : ""}
                 {newArrItem[1]}{" "}
-                {page[idx].page ? <LinkButton artist={page[idx].artist} /> : ""}
+                {page[idx].page ? <LinkButton artist={page[idx].artist} /> : ""} */}
+                <ArtistLinkButton
+                  artist={newArrItem[1]}
+                  page={page[idx]}
+                  extra={newArrItem[0]}
+                />
               </div>
             );
           }
@@ -78,8 +85,9 @@ export const EditedTitle = ({ artist, page }) => {
   } else {
     return (
       <div className="flex items-center font-poppins font-light lg:text-2xl xl:text-3xl relative">
-        {artist}
-        {page[0].page ? <LinkButton artist={page[0].artist} /> : ""}
+        {/* {artist}
+        {page[0].page ? <LinkButton artist={page[0].artist} /> : ""} */}
+        <ArtistLinkButton artist={artist} page={page[0]} />
       </div>
     );
   }
