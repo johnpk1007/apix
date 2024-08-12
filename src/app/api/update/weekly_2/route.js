c2f696a953795ded762b24be6a4accd7b9f8c526
John <johnpk1007@gmail.com>
2024-08-12 13:40:51 +0900

    experiment

--- src/app/api/update/weekly_2/route.js
+++ src/app/api/update/weekly_2/route.js
@@ -7,31 +7,34 @@ export const dynamic = "force-dynamic";
 
 export async function POST(request) {
   console.log("api/update/weekly_2 starts working");
-  // const data = await request.json();
-  // const headers = request.headers;
-  // const request_type = headers.get("request_type");
+  const data = await request.json();
+  const headers = request.headers;
+  const request_type = headers.get("request_type");
   const response = new Response(
     JSON.stringify({ message: "Request received. Processing in background." })
   );
-  // if (request_type === "send_data") {
-  //   const list = dataToArtistList(data);
-  //   axios.post(
-  //     `${process.env.NEXT_PUBLIC_BASE_URL}/api/update/artist_scrape`,
-  //     JSON.stringify({ list: list, operationArray: [], num: 0 })
-  //   );
-  // } else if (request_type === "return_data") {
-  //   axios.post(
-  //     `${process.env.NEXT_PUBLIC_BASE_URL}/api/update/artist_scrape`,
-  //     JSON.stringify({ list: list, operationArray, num })
-  //   );
-  // } else if (request_type === "return_data_final") {
-  //   try {
-  //     await connectToDB();
-  //     billboardArtistInsertion(operationArray);
-  //     axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/update/weekly_3`);
-  //   } catch (error) {
-  //     console.error(error.message);
-  //   }
-  // }
+  if (request_type === "send_data") {
+    const list = dataToArtistList(data);
+    axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/update/artist_scrape`,
+      JSON.stringify({ list: list, operationArray: [], num: 0 })
+    );
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  } else if (request_type === "return_data") {
+    axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/update/artist_scrape`,
+      JSON.stringify({ list: list, operationArray, num })
+    );
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  } else if (request_type === "return_data_final") {
+    try {
+      await connectToDB();
+      billboardArtistInsertion(operationArray);
+      axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/update/weekly_3`);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
   return response;
 }
