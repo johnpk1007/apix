2dc35d22a2401cdd0e59331c7d21c804ef668c8b
John <johnpk1007@gmail.com>
2024-08-29 10:08:24 +0900

    add open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,18 @@ import { Provider } from "../../components/provider";
 export const metadata = {
   title: "API X API",
   description: "Collaboration of two great APIs",
+  openGraph: {
+    title: "API X API",
+    description: "Collaboration of two great APIs",
+    images: [
+      {
+        url: "https://www.apixapi.xyz/ogImage.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
 };
 
 const poppins = Poppins({
